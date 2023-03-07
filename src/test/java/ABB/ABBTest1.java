a8dbd9bd8de3b4b8e4b3d9edeb1c68e79a638be8
Aishwarya <Aishwarya@LAPTOP-3OOOK5AL>
2023-03-07 22:46:13 +0530

    ABB new

---
+++ src/test/java/ABB/ABBTest1.java
@@ -0,0 +1,24 @@
+package ABB;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ABBTest1 {
+	@Test
+	public void tc1()
+	{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+
+}
